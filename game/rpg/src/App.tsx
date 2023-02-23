d62236f23dc01088a64766c992a22a29d97b6579
Lucas Soares <113461420+caslulucas@users.noreply.github.com>
2023-02-22 23:37:47 -0300

    First Commit

---
+++ game/rpg/src/App.tsx
@@ -0,0 +1,55 @@
+import { useEffect } from 'react'
+import * as C from './App.styles'
+import { Character } from './components/Character'
+import { useCharacter } from './hooks/useCharacter'
+
+
+const App = () => {
+  const char = useCharacter('Player 1 ')
+  const char2 = useCharacter('Player 2')
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown)
+  }, [])
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    switch(e.code) {
+      case 'keyA':
+      case 'ArrowLeft':
+        char.moveLeft()
+
+      break;
+      case 'keyW':
+      case 'ArrowUp':
+        char.moveUp()
+
+      break;
+      case 'keyD':
+      case 'ArrowRight':
+        char.moveRight()
+
+      break;
+      case 'keyS':
+      case 'ArrowDown':
+        char.moveDown()
+
+      break;
+    }
+  }
+
+  return (
+    <C.Container>
+      <C.Map>
+        <Character x={char.x} y={char.y} side={char.side} name={char.name} />
+        <Character x={char2.x} y={char2.y} side={char2.side} name={char2.name}/>
+      </C.Map>
+      <button onClick={()=>char2.moveLeft()}>Esquerda</button>
+      <button onClick={()=>char2.moveRight()}>Direita</button>
+      <button onClick={()=>char2.moveUp()}>Cima</button>
+      <button onClick={()=>char2.moveDown()}>Baixo</button>
+    </C.Container>
+
+  )
+}
+
+export default App
